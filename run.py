7c467b84b82fd4d6c751b4df8197341051a6a044
Dmitry Sedov <sedovof@yandex.ru>
2021-01-26 15:14:24 +0300

    made first corrected test

--- run.py
+++ run.py
@@ -1,38 +1,40 @@
 import subprocess
 import multiprocessing
 import sys
-
+from random import randint
 #thread_num
 #use_gpu
 #plate_max_width
 #plate_min_width
-num_threads = multiprocessing.cpu_count()
+num_threads = 8#multiprocessing.cpu_count()
 print("Start testing.\n")
-print("System has " + num_threads + " threads.\n");
+print("System has " + str(num_threads) + " threads.\n")
 
-def main(videoName):
+def main(execName, videoName):
 	data = dict()
-	for i in range(1, num_threads):
-		data["thread_num"] = i
+	for i in range(1, num_threads+1):
+		data["thread_num"] = i#randint(1, num_threads)
 		setupDataFile = "setupData.txt"
 		outputDataFile = "outputFile.txt"
 		with open(setupDataFile, 'w') as file_object:
-			whole_str = "thread_num|" + i + "\n"
+			whole_str = "thread_num|" + str(data["thread_num"]) + "\n"
 			file_object.write(whole_str)
-		programma = "./minimal_lpr " + videoName +" " + setupDataFile +" " + outputDataFile
-		process = subprocess.run(programma)
+		process = subprocess.run([execName, videoName, setupDataFile, outputDataFile])
 		if process.returncode == 1:
 			print("something wrong...")
-			return 1;
-		fps = 0.0;
+			return 1
+		outData = dict()
 		with open(outputDataFile) as file_object:
-			lines = file_object.readlines();
+			lines = file_object.readlines()
 			for line in lines:
 				dt = line.rstrip().split("|")
 				print(dt[0]+ " = "  + dt[1])
-				fps = dt[1]
+				outData[dt[0]] = dt[1]
 		with open("result.csv", "a") as file_object:
-			whole_str = i + ";" + fps + "\n"
+			whole_str = str(data["thread_num"])# + ";" + str(fps) + "\n"
+			for key, value in outData.items():
+				whole_str  = whole_str + ";" + str(value)
+			whole_str += "\n"
 			file_object.write(whole_str)
 
 			
@@ -42,7 +44,8 @@ if __name__ == '__main__':
 	if (len(sys.argv) < 2):
 		print("Need define video file.")
 	else:
-		main(sys.argv[1])
+		main(sys.argv[1], sys.argv[2])
+	
 
 
 print("Finish testing.\n")
